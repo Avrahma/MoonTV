49fc8390dd4bce1d46268a2701a7b75ddbb96bb9
Avrahma <165910723+Avrahma@users.noreply.github.com>
2025-07-06 17:01:20 +0800

    Update MobileBottomNav.tsx

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -64,12 +64,12 @@ const MobileBottomNav = ({ activePath }: MobileBottomNavProps) => {
     { icon: VenetianMask, label: '日漫', href: '/douban?type=tv&tag=日本动画' },
   ];
 
-  const { siteName } = useSite();
-  if (siteName !== 'Youtube') {
-    navItems.push({
-      icon: Github,
-      label: 'Youtube',
-      href: 'https://www.youtube.com/',
+  //const { siteName } = useSite();
+  //if (siteName !== 'Youtube') {
+    //navItems.push({
+      //icon: Github,
+      //label: 'Youtube',
+      //href: 'https://www.youtube.com/',
     });
   }
 
